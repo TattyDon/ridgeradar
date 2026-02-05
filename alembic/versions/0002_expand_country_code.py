0fc5cb009d46f27bde5fe720d1dcaca8a402eda8
TattyDon <john.laverick@gmail.com>
2026-02-05 08:57:05 +0000

    Add migration files for alembic chain

---
+++ alembic/versions/0002_expand_country_code.py
@@ -0,0 +1,27 @@
+"""Expand country_code column and add competition_stats table.
+
+Revision ID: 0002
+Revises: 0001
+Create Date: 2025-01-15 00:00:00.000000
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '0002'
+down_revision: Union[str, None] = '0001'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Already applied - tables exist
+    pass
+
+
+def downgrade() -> None:
+    pass
